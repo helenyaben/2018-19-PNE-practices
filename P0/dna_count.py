aa641768f13cb9a8eb01669abe448e70f7028cbb
hyaben <helena-99@hotmail.com>
2019-04-09 13:02:20 +0200

    Practice 0

---
+++ P0/dna_count.py
@@ -0,0 +1,20 @@
+# We are gonna find a DNA sequence, measure its length and count the basis.
+
+seq = input('Enter a valid sequence to proceed: ')
+
+seq = seq.upper()
+
+len_seq = int(len(seq))
+
+print('The length of the seq is : ', len_seq)
+
+print('The number of adenine nucleotides is: ', seq.count('A'))
+print('The number of guanine nucleotides is :', seq.count('G'))
+print('The number of thymine nucleotides is:  ', seq.count('T'))
+print('The number of cytosine nucleotides is: ', seq.count('C'))
+print('The number of uracil nucleotides is: ', seq.count('U'))
+
+a = int(seq.count('A'))
+
+percentage = a/len_seq
+print(percentage)
---
+++ P0/hello_world.py
@@ -0,0 +1,4 @@
+print('Hello world')
+print('You are doing amazing sweetie')
+print('Haters gonna hate')
+print('Thank you, next')
